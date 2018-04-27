532efb165a8921dcd1047d7ab9ee4b234c6677f7
Lifeng Li <lifli@cisco.com>
2018-04-26 19:51:48 -0700

    add memorization for fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,6 +1,6 @@
 // O(N) n is the position, start at 1, (not index) in the sequence
 const fibonacci = ( n ) => {
-    if (n < 2) { return 1; }
+    if (n < 3) { return 1; }
     
     let f = [0, 1];
     // linear time complexity: for n elements it will require n-1 iterations
@@ -11,9 +11,10 @@ const fibonacci = ( n ) => {
     return f.pop();
 };
 
-// O(2^N)
+// O(N^2)
 // exponential time complexity: for n elements it will require nearly 2~n recursions
 // the exact recursion = 2 * the calculated Fibonacci value
+// if index start at 0, use if(n<3)
 const fibonacci2 = ( n ) => {
     if(n <= 3) {
         return 1;
@@ -25,6 +26,7 @@ const fibonacci2 = ( n ) => {
 // O(N)
 // this essentially equivalent to a for loop
 // linear time complexity: for n elements it will require n-1 iterations
+// if index start at 0, use if(n >= 3)
 const fibonacci3 = (a, b, n) => { 
     if(n > 3) { 
         return fibonacci3(b, a + b, n - 1); 
@@ -35,10 +37,22 @@ const fibonacci3 = (a, b, n) => {
 };
 
 // O(N) the es6 way
+// if index start at 0, use (n-- >= 2)
 const fibonacci4 = (n) => {
     let first = 0, second = 1;
     while(n-- > 2){
         [first, second] = [second, first + second];
     }
     return second;
+};
+
+// O(N) use memorization
+// if index start at 0, use if(n<3)
+const fibonacci5 = (n, m={}) => {
+    if(n <= 3) { return 1; }
+    if(m[n]) { return m[n]; }
+    let fib = fibonacci5(n-1, m) + fibonacci5(n-2, m);
+    m[n] = fib;
+
+    return fib;
 };
