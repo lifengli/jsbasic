1af7668216a99acb3537ce3797760b6eee570dbc
Lifeng Li <lifli@cisco.com>
2018-05-02 13:36:00 -0700

    update comments

--- sort/insertion_sort2.js
+++ sort/insertion_sort2.js
@@ -2,7 +2,7 @@
 const insertionSort = (array) => {
     //start from 2nd place, then move to the right
     for(let index = 1; index < array.length; index++){
-        //current index value, to be inserted into the "new sorted array", on right-most position
+        //current index value, to be inserted into the "new sorted array", starting at the right-most position
         let value = array[index];
         //inner loop start from 1 spot to the left of to-be-inserted value
         let i = index - 1;
@@ -11,8 +11,8 @@ const insertionSort = (array) => {
             //swap if to-be-inserted value is less the index value to is left
             array[i+1] = array[i];
         }
-        // i stops at either -1, or the (index - 1) a value that is equal or bigger than the to-be-inserted value
-        // drop the "value" at that position
+        // i stops at either -1, or the index of the value that is equal or smaller than the to-be-inserted value
+        // drop the "value" at one position to the right
         array[i+1] = value;
     }
 };
