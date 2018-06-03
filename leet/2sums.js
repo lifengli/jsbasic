ac3e041f87b9699b87bb53decea76ee8229c77ac
Lifeng Li <lifli@cisco.com>
2018-06-02 18:05:38 -0700

    add set to 2sums

--- leet/2sums.js
+++ leet/2sums.js
@@ -1,18 +1,37 @@
 const nums = [2, 4, 7, 11, 15, 18, 22, 25, 29, 32, 36, 40, 45, 50, 100];
 const target = 150;
 
-//O(N) - better
-let temp = [];
+//O(N)
 const twoSum = (nums, target) => {
+    const temp = [];
     //run through the array only once, worst case nums.length
     for(let i=0; i<nums.length; i++){
         if(temp.indexOf(target-nums[i] < 0)){
             temp.push(target-nums[i]);
         }
         if(temp.indexOf(nums[i]) >= 0){
+            if(nums.indexOf(target - nums[i]) === i) continue;
             return [nums.indexOf(target - nums[i]), i];
         }
     }
+    return false;
+};
+
+//O(N) - better
+const twoSum1 = (list, target) => {
+    const checker = new Set();
+
+    for(let i = 0; i < list.length; i++){
+        const one = list[i];
+        const match = target - one;
+
+        if(checker.has(match)){
+	    return [list.indexOf(match), i];
+	}
+
+        checker.add(one);
+    }
+
     return false;
 };
 
