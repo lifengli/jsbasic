65b9769253ae5d12f3e529552dc80691272cb321
Lifeng Li <lifli@cisco.com>
2018-06-09 00:47:19 -0700

    update selection sort

--- sort/selection_sort2.js
+++ sort/selection_sort2.js
@@ -4,19 +4,17 @@ let array = [22, 11, 0, 99, 1, 88, -2, 7, 42, 1, "c", "z", "A", "h3"];
 const selectionSort = (array) => {
     for(let i = 0; i < array.length; i++) {
         let minIndex = i;
-        let minValue = array[i];
         
         //always find the smallest value in the sub-array (starting form i+1)
         for(let j = minIndex+1; j < array.length; j++){
           if(array[j] < array[minIndex]){
             minIndex = j;
-            minValue = array[j];
           }
         }
 
         //swap the values between current position and the smallest
         let temp = array[i];
-        array[i] = minValue;
+        array[i] = array[minIndex];
         array[minIndex] = temp;
     }
 };
